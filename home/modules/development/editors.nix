940838e5580748943333352c2c3854f23008c68d
Ahmed-chouaya <chouaya.ahmed83@gmail.com>
2026-01-29 15:34:39 +0100

    fix: correct relative path to dotfiles in editors.nix

--- home/modules/development/editors.nix
+++ home/modules/development/editors.nix
@@ -3,13 +3,13 @@
 {
   # Copy helix configuration (to be populated in Task 28)
   home.file.".config/helix" = {
-    source = ../dotfiles/helix;
+    source = ../../dotfiles/helix;
     recursive = true;
   };
 
   # Copy opencode configuration (to be populated in Task 28)
   home.file.".config/opencode" = {
-    source = ../dotfiles/opencode;
+    source = ../../dotfiles/opencode;
     recursive = true;
   };
 }
