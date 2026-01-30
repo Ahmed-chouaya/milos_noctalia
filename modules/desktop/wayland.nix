78bfd66995eded6de9308a66c0eb182b8c904634
Ahmed-chouaya <chouaya.ahmed83@gmail.com>
2026-01-30 21:57:25 +0100

    fix: ensure default portal settings for Niri use lib.mkForce

--- modules/desktop/wayland.nix
+++ modules/desktop/wayland.nix
@@ -20,7 +20,7 @@
         default = [ "gtk" ];
       };
       niri = {
-        default = [ "wlr" "gtk" ];
+        default = lib.mkForce [ "wlr" "gtk" ];
         "org.freedesktop.impl.portal.ScreenCast" = [ "wlr" ];
         "org.freedesktop.impl.portal.Screenshot" = [ "wlr" ];
       };
