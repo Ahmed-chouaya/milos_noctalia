8e9532266c6be9e0643e1ee25a6aa16e5824208f
Ahmed-chouaya <chouaya.ahmed83@gmail.com>
2026-02-01 15:23:32 +0100

    fix: remove helix configuration to prevent conflicts with noctalia's theme management

--- home/modules/development/editors.nix
+++ home/modules/development/editors.nix
@@ -1,11 +1,9 @@
 { config, pkgs, ... }:
 
 {
-  # Copy helix configuration (to be populated in Task 28)
-  home.file.".config/helix" = {
-    source = ../../dotfiles/helix;
-    recursive = true;
-  };
+  # Note: Helix configuration is managed by noctalia-shell's template system
+  # to enable dynamic theme switching. Do not add home.file.".config/helix" here
+  # as it conflicts with noctalia's ability to write theme files.
 
   # Copy opencode configuration (to be populated in Task 28)
   home.file.".config/opencode" = {
